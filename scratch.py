0a25a32f03f295f65f1808cdf531797bef84cea2
anastasyaginting <56407197+anastasyaginting@users.noreply.github.com>
2019-11-02 08:02:42 +0700

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,11 @@
+#Ini Variabel & Nilai
+
+namalengkap = "Anastasya Br Ginting"
+namapanggilan = "Tasya"
+npm = "311910528"
+tempatlahir = "Kutatengah"
+umur = "19"
+telf = "081262105482"
+alamat = "Bekasi"
+
+print("assalamualaikum, \n let me introduce my self, My name is",namalengkap,",but you call me",namapanggilan,".My NPM is",npm,". I was born in",tempatlahir,"and i am",umur,"years old. I am very glad if you want to invite my house in",alamat,". So, dont't forget to call me before with the number",telf,"\n Thank You")
